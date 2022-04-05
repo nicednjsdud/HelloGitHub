0bfe60172df632de3dae7eff5122aebdffc280a4
nicednjsdud <nicednjsdud@naver.com>
2022-04-05 14:15:01 +0900

    Merge branch 'ezen'

--- src/src/kr/co/ezenac/HelloGit.java
+++ src/src/kr/co/ezenac/HelloGit.java
@@ -2,6 +2,7 @@ package kr.co.ezenac;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("What's git?");
+		System.out.println("Hello git!");
+		System.out.println("What's Git?");
 	}
 }
